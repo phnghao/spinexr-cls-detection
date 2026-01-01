a9750990005aa45967d44b5a34a0f4479f162a0b
phnghao <phnghao2005@gmail.com>
2026-01-01 10:59:01 +0700

    + detect_get_label.bat

--- detect_get_label.bat
+++ detect_get_label.bat
@@ -10,3 +10,8 @@ python -m spine.detection.gen_label ^
     --anno-file "./data/detection/val.csv" ^
     --output-dir "./data/detection/yolo/val/labels" ^
     --cpus 4
+
+python -m spine.detection.gen_label ^
+    --anno-file "./data/test_prepro_data.csv" ^
+    --output-dir "./data/detection/yolo/test/labels" ^
+    --cpus 4
