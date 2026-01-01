4e7dca019e7ef7934310a8d8f87bdf6fe5c14a23
phnghao <phnghao2005@gmail.com>
2026-01-01 11:22:14 +0700

    +detect_train.bat

--- detect_train.bat
+++ detect_train.bat
@@ -1,9 +1,9 @@
 @echo off
 
-echo train Yolov8
+echo train yolov8
 
 python -m spine.detection.train_yolo ^
-    --yaml-file "./data/detection/yolo/data.yaml" ^
+    --yaml-file "./data/detection/yolo/train.yaml" ^
     --epochs 1 ^
     --batch-size 4 ^
     --imgsz 640 ^
