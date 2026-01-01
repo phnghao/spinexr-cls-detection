4347d55e16b08390a7e799fb8839bd841e0d93bb
phnghao <phnghao2005@gmail.com>
2026-01-01 11:16:56 +0700

    +test.yaml

--- detect_make_yaml.bat
+++ detect_make_yaml.bat
@@ -1,14 +1,22 @@
 @echo off
-echo Created data.yaml
+echo Created train.yaml
 
 set ROOT_DIR=./data/detection/yolo
 set TRAIN_IMAGES=train/images
 set VAL_IMAGES=val/images
-set OUTPUT=data.yaml
+set OUTPUT=train.yaml
 
-python -m spine.detection.make_yaml ^
+python -m spine.detection.make_train_yaml ^
     --root-dir %ROOT_DIR% ^
     --train-images %TRAIN_IMAGES% ^
     --val-images %VAL_IMAGES% ^
     --output %OUTPUT%
+
+echo Created test.yaml
+python -m spine.detection.make_test_yaml ^
+    --root-dir "./data/detection/yolo/test" ^
+    --train-images "images" ^
+    --val-images "images" ^
+    --test-images "images" ^
+    --output "./data/detection/yolo/test.yaml"
 
---
+++ spine/detection/make_test_yaml.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+import yaml
+import argparse
+
+def make_data_yaml(root_dir, train_images, val_images, test_images,out, names = None):
+    root_dir = Path(root_dir)
+
+    if names is None:
+        raise ValueError("Class names list must not be None")
+    
+    data = {
+        'path':root_dir.resolve().as_posix(),
+        'train':train_images,
+        'val':val_images,
+        'test':test_images,
+        'nc':len(names),
+        'names':names
+    }
+
+    out_path = out
+
+    with open(out_path, 'w', encoding='utf-8') as f:
+        yaml.safe_dump(data,f ,sort_keys=False)
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--root-dir', required = True, type = str)
+    parser.add_argument('--train-images', required= True, type = str)
+    parser.add_argument('--val-images', required = True, type = str)
+    parser.add_argument('--test-images',required=True, type = str)
+    parser.add_argument('--output', required = True, type = str)
+
+    args  = parser.parse_args()
+
+    names = [
+        "Osteophytes",
+        "Spondylolysthesis",
+        "Disc space narrowing",
+        "Vertebral collapse",
+        "Foraminal stenosis",
+        "Surgical implant",
+        "Other lesions",
+    ]
+
+    make_data_yaml(
+        root_dir = args.root_dir,
+        train_images = args.train_images,
+        val_images= args.val_images,
+        test_images=args.test_images,
+        out = args.output,
+        names = names
+    )
+
+if __name__ == '__main__':
+    main()
+
+
