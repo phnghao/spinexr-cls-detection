0887c1ef7f751f03828d2ef7354a866482883c22
phnghao <phnghao2005@gmail.com>
2026-01-05 01:23:10 +0700

    update batch script

--- detect_train.bat
+++ detect_train.bat
@@ -4,7 +4,15 @@ echo train yolov8
 
 python -m spine.detection.train_yolo ^
     --yaml-file "./data/detection/yolo/train.yaml" ^
-    --epochs 124 ^
+    --epochs 120 ^
     --batch-size 16 ^
     --imgsz 640 ^
     --device 0 
+
+REM train yolo with batch 32
+REM python -m spine.detection.train_yolo ^
+REM     --yaml-file "./data/detection/yolo/train.yaml" ^
+REM     --epochs 239 ^
+REM     --batch-size 16 ^
+REM     --imgsz 640 ^
+REM     --device 0
