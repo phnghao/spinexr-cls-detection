5c7888cb7c73b8c02dcca743d01a252bf17d3cf8
phnghao <phnghao2005@gmail.com>
2026-01-05 01:13:15 +0700

    fine-tuned num_epochs = 45

--- cls_train_net.bat
+++ cls_train_net.bat
@@ -4,7 +4,7 @@ python -m spine.classification.train_net ^
     --csv-file "./data/classification/train.csv" ^
     --image-dir "./data/train_pngs" ^
     --save-file "./cls_save/train/best.pth" ^
-    --nepochs 46 ^
+    --nepochs 45 ^
     --batch-size 32 ^
     --num-workers 4 ^
     --image-size 224
