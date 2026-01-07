8a976b60690fabe1caba85f455886327d4be1036
phnghao <phnghao2005@gmail.com>
2026-01-07 12:11:47 +0700

    delete model yolo

--- detect_eval.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo eval yolov8
-
-python -m spine.detection.eval ^
-    --weights-path "./runs/detect/train/weights/best.pt" ^
-    --data-yaml "./data/detection/yolo/test.yaml"
--- detect_split_data.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo split data
-python -m spine.detection.split_data ^
-    --csv-file "./data/train_prepro_data.csv" ^
-    --train-output "./data/detection/train.csv" ^
-    --val-output "./data/detection/val.csv" 
