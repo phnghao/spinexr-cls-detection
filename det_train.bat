9cd57187bb5bc4034f63e75c87897aa7e3150db2
phnghao <phnghao2005@gmail.com>
2026-01-08 22:29:48 +0700

    update train sparse rcnn

--- det_train.bat
+++ det_train.bat
@@ -2,5 +2,5 @@
 
 echo training sparse rcnn
 python -m spine.sparsercnn.train ^
-    --config-file "./spine/configs/sparsercnn.res101.300pro.3x.yaml" ^
+    --config-file "./spine/configs/sparsercnn.yaml" ^
     --num-gpus 1
